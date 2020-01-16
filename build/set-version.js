0da7407085f41446d88e723a83fdf9250ae24542
Carlos Jeurissen <1038267+CarlosJeurissen@users.noreply.github.com>
2020-01-16 09:25:51 +0100

    update to v1.2.4, update dependencies, update csp version

--- build/set-version.js
+++ build/set-version.js
@@ -1,5 +1,5 @@
 const fs = require('fs')
-const version = process.argv[2]
+const newVersion = process.argv[2]
 const files = [
   'package.json',
   'src/manifest.json',
@@ -7,12 +7,28 @@ const files = [
   'src/chromeapp/manifest.json'
 ]
 
+function setReportVersion (csp) {
+  return csp.replace(/\/v\d[\d_]+\d\//, '/v' + newVersion.replace(/\./g, '_') + '/')
+}
+
+function processCsp (json) {
+  const cspObj = json.content_security_policy
+  if (typeof cspObj === 'object') {
+    Object.keys(cspObj).forEach(function (key) {
+      cspObj[key] = setReportVersion(cspObj[key])
+    })
+  } else if (typeof cspObj === 'string') {
+    json.content_security_policy = setReportVersion(cspObj)
+  }
+}
+
 files.forEach((file) => {
   fs.readFile(file, 'utf8', function (err, content) {
-    if (err) return
-    let parseJson = JSON.parse(content)
-    parseJson.version = version
-    fs.writeFile(file, JSON.stringify(parseJson, null, '  '), 'utf8', function (err) {
+    if (err || !content) return
+    const parsedJson = JSON.parse(content)
+    parsedJson.version = newVersion
+    processCsp(parsedJson)
+    fs.writeFile(file, JSON.stringify(parsedJson, null, '  '), 'utf8', function (err) {
       if (err) throw err
     })
   })
